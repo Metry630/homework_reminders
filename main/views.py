d29ddee69bcd41bca66687a44b3c54a065bff918
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2023-09-11 19:24:49 +0700

    mengganti name dan description

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
+        'name': 'Oey Joshua Jodrian - PBP D',
         'amount': 3,
-        'description': 'Tugas Kuliah'
+        'description': 'Homework Reminders'
     }
 
     return render(request, "main.html", context)
