ddb14035afcdea2b8df6140cd3bbeae230138e12
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2023-10-13 09:24:43 +0700

    add trusted hosts

--- homework_reminders/settings.py
+++ homework_reminders/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ['https://homeworkreminders.up.railway.app', 'http://127.0.0.1:8000/']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
